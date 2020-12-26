05dc9aa60055b318625e40cec8141fa1fa69054c
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2020-12-26 21:36:52 +0100

    initial models

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import argparse
 # External imports
 # Local imports
 import data
+import models
 
 
 def train(args):
---
+++ models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Standard imports
+# External imports
+import torch.nn as nn
+
+
+def Model(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
