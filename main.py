309940207753d3c88ff1103076f7257c638f6ce0
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2020-12-26 22:59:03 +0100

    main train loop for testing the model implementation

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 import logging
 import argparse
 # External imports
+import torch
+import tqdm
 # Local imports
 import data
 import models
@@ -17,10 +19,19 @@ def train(args):
     logger = logging.getLogger(__name__)
     logger.info("Training")
 
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
     # Data loading
     train_loader, valid_loader, test_loader = data.get_dataloaders(data._DEFAULT_COMMONVOICE_ROOT,
                                                                   n_threads=args.nthreads)
+    n_mels = data._DEFAULT_NUM_MELS
+    model = models.Model(n_mels)
+    model.to(device)
 
+    for X, y in tqdm.tqdm(train_loader):
+        X, y = X.to(device), y.to(device)
+
+        model(X)
 
 def test(args):
     """
